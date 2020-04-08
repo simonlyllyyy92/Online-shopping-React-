d62115501420d190bcb414d4f3472e8663530d86
simon <simonlyllyyy@gmail.com>
2020-04-08 16:01:22 +1000

    add header

--- online-shopping/src/App.js
+++ online-shopping/src/App.js
@@ -4,10 +4,13 @@ import {Route, Switch} from 'react-router-dom'
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component'
+import Header from './components/header/header.component'
 
 function App() {
+  // No matter what header will always render
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path = '/' component = {HomePage} />
         <Route path = '/shop' component = {ShopPage} />
