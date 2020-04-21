479d800f8afd1462ee7244afa57188ad0fddba86
simon <simonlyllyyy@gmail.com>
2020-04-21 15:58:25 +1000

    add cart item and cart item redux set up

---
+++ online-shopping/src/redux/cart/cart.utils.js
@@ -0,0 +1,11 @@
+export const addItemToCart = (cartItems, cartItemToAdd) => {
+    const existingCardItem = cartItems.find(cartItem=> cartItem.id  === cartItemToAdd.id)
+    if(existingCardItem){
+        return cartItems.map(cartItem => 
+                cartItem.id === cartItem.id ? {...cartItem, quantity: cartItem.quantity + 1}
+                : {cartItem}
+            )
+    }
+
+    return [...cartItems, {...cartItemToAdd, quantity: 1}]
+}
